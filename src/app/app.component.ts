4f138b95efbeefa5ea2cbceb35a95a1224ab0316
CLEOCIMAR GOMES DA SILVA <cleocimar@users.noreply.github.com>
2024-11-07 01:41:15 -0400

    limpa outlet quando seleciona o menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
+import { Router, Routes } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatSidenavModule } from '@angular/material/sidenav'; 
+import { MatListModule } from '@angular/material/list'; 
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconButton} from '@angular/material/button';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
+import {MatDrawerContainer} from '@angular/material/sidenav';
+import { ClienteComponent } from './cliente/cliente.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule, 
+    MatListModule, 
+    MatButtonModule,
+    MatIconButton,
+    MatSidenavModule,
+    MatIconModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'base';
+  showContent = true;
+  constructor(private router: Router) {
+
+  } 
+
+  navigate(path: string) { 
+    this.showContent = true;
+    this.router.navigate([path]); 
+  }
+
+  toggleDrawer(drawerOpened: boolean){
+    this.showContent = !drawerOpened;    
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { ClienteComponent } from './cliente/cliente.component';
+import { FornecedorComponent } from './fornecedor/fornecedor.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'app', component: AppComponent }, 
+    { path: 'cliente', component: ClienteComponent }, 
+    { path: 'fornecedor', component: FornecedorComponent }, 
+    { path: '', redirectTo: '/cliente', pathMatch: 'full' }
+];
+
+@NgModule({ 
+    imports: [RouterModule.forRoot(routes)], 
+    exports: [RouterModule] 
+}) 
+
+export class AppRoutingModule { }
+
